4d7a44c4f69d61d808b86a90483cc9323d6a0220
monkeyman707 <Monkey_man_707@live.com>
2016-04-05 20:26:05 +1200

    Created single menu screen with progressive menu

--- app/src/main/java/com/cryptolearner/mobile/cryptolearner/MainActivity.java
+++ app/src/main/java/com/cryptolearner/mobile/cryptolearner/MainActivity.java
@@ -2,13 +2,11 @@ package com.cryptolearner.mobile.cryptolearner;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -51,8 +49,87 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void caesar(View view) {
-        Intent intent = new Intent(this, CaesarMenu.class);
+    public void onClickCaesar(View view) {
+        View caesar1 = findViewById(R.id.CaesarLvl1Btn);
+        if (caesar1.getVisibility() == View.VISIBLE) {
+            caesarGone(true);
+        } else {
+            caesarGone(false);
+        }
+        //Intent intent = new Intent(this, CaesarMenu.class);
+        //startActivity(intent);
+    }
+
+    public void onClickSubstitution(View view) {
+        if (findViewById(R.id.SubLvl1Btn).getVisibility() == View.VISIBLE) {
+            subGone(true);
+        } else {
+            subGone(false);
+        }
+    }
+
+    public void onClickVigenere(View view) {
+        if (findViewById(R.id.VigenereLvl1Btn).getVisibility() == View.VISIBLE) {
+            vigenereGone(true);
+        } else {
+            vigenereGone(false);
+        }
+    }
+
+    public void onClickChallenge(View view) {
+        Intent intent = new Intent(this, DummyActivity.class);
         startActivity(intent);
+    }
+
+
+    private void caesarGone(boolean setGone) {
+        View caesar1 = findViewById(R.id.CaesarLvl1Btn);
+        View caesar2 = findViewById(R.id.CaesarLvl2Btn);
+        View caesar3 = findViewById(R.id.CaesarLvl3Btn);
+        if (setGone) {
+            caesar1.setVisibility(View.GONE);
+            caesar2.setVisibility(View.GONE);
+            caesar3.setVisibility(View.GONE);
+        } else {
+            caesar1.setVisibility(View.VISIBLE);
+            caesar2.setVisibility(View.VISIBLE);
+            caesar3.setVisibility(View.VISIBLE);
+            subGone(true);
+            vigenereGone(true);
+        }
+    }
+
+    private void subGone(boolean setGone) {
+        View btn1 = findViewById(R.id.SubLvl1Btn);
+        View btn2 = findViewById(R.id.SubLvl2Btn);
+        View btn3 = findViewById(R.id.SubLvl3Btn);
+        if (setGone) {
+            btn1.setVisibility(View.GONE);
+            btn2.setVisibility(View.GONE);
+            btn3.setVisibility(View.GONE);
+        } else {
+            btn1.setVisibility(View.VISIBLE);
+            btn2.setVisibility(View.VISIBLE);
+            btn3.setVisibility(View.VISIBLE);
+            caesarGone(true);
+            vigenereGone(true);
+        }
+    }
+
+    private void vigenereGone(boolean setGone) {
+        View btn1 = findViewById(R.id.VigenereLvl1Btn);
+        View btn2 = findViewById(R.id.VigenereLvl2Btn);
+        View btn3 = findViewById(R.id.VigenereLvl3Btn);
+        if (setGone) {
+            btn1.setVisibility(View.GONE);
+            btn2.setVisibility(View.GONE);
+            btn3.setVisibility(View.GONE);
+        } else {
+            btn1.setVisibility(View.VISIBLE);
+            btn2.setVisibility(View.VISIBLE);
+            btn3.setVisibility(View.VISIBLE);
+            caesarGone(true);
+            subGone(true);
+        }
     }
 }
