11792b8479f4fbbb462050cf5d8b88de8d2b506c
monkeyman707 <Monkey_man_707@live.com>
2016-04-26 15:20:42 +1200

    Added spinnable wheel to dummy activity. This is foundation for cipher wheel

--- app/src/main/java/com/cryptolearner/mobile/cryptolearner/DummyActivity.java
+++ app/src/main/java/com/cryptolearner/mobile/cryptolearner/DummyActivity.java
@@ -1,11 +1,19 @@
 package com.cryptolearner.mobile.cryptolearner;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.lukedeighton.wheelview.WheelView;
+import com.lukedeighton.wheelview.adapter.WheelAdapter;
 
 public class DummyActivity extends AppCompatActivity {
 
@@ -15,6 +23,37 @@ public class DummyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dummy);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+//        WheelView wheelView = (WheelView) findViewById(R.id.wheelview);
+//        if (wheelView == null) {} else {
+//            wheelView.setAdapter(new WheelAdapter() {
+//                @Override
+//                public Drawable getDrawable(int position) {
+//                    Drawable[] drawable = new Drawable[]{
+//                            createOvalDrawable(Color.parseColor("#356723")),
+//                            //new TextDrawable(String.valueOf(position))
+//                    };
+//                    return new LayerDrawable(drawable);
+//                }
+//
+//                private Drawable createOvalDrawable(int color) {
+//                    ShapeDrawable shapeDrawable = new ShapeDrawable(new OvalShape());
+//                    shapeDrawable.getPaint().setColor(color);
+//                    return shapeDrawable;
+//                }
+//
+//                @Override
+//                public Object getItem(int position) {
+//                    return null;
+//                }
+//
+//                @Override
+//                public int getCount() {
+//                    //return the count
+//                    return 1;
+//                }
+//            });
+//        }
     }
 
 }
---
+++ app/src/main/java/com/cryptolearner/mobile/cryptolearner/RotaryDialerView.java
@@ -0,0 +1,134 @@
+package com.cryptolearner.mobile.cryptolearner;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+
+
+/**
+ * Adapted from a tutorial at http://www.ssaurel.com/blog/how-to-create-a-rotary-dialer-application-for-android/
+ */
+public class RotaryDialerView extends View {
+    private float rotorAngle;
+    private final Drawable rotorDrawable;
+    private final int r1 = 50;
+    private final int r2 = 230;
+    private double lastFi;
+
+    public RotaryDialerView(Context context) {
+        this(context, null);
+    }
+
+    public RotaryDialerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RotaryDialerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        rotorDrawable = context.getResources().getDrawable(R.drawable.dialer);
+    }
+
+    private void fireDialListenerEvent(int number) {
+        // TODO fire dial event
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int availableWidth = getRight() - getLeft();
+        int availableHeight = getBottom() - getTop();
+
+        int x = availableWidth / 2;
+        int y = availableHeight / 2;
+
+        canvas.save();
+
+        rotorDrawable.setBounds(0, 0, rotorDrawable.getIntrinsicWidth(),
+                rotorDrawable.getIntrinsicHeight());
+
+        if (rotorAngle != 0) {
+            canvas.rotate(rotorAngle, x, y);
+        }
+
+        rotorDrawable.draw(canvas);
+
+        canvas.restore();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final float x0 = getWidth() / 2;
+        final float y0 = getHeight() / 2;
+        float x1 = event.getX();
+        float y1 = event.getY();
+        float x = x0 - x1;
+        float y = y0 - y1;
+        double r = Math.sqrt(x * x + y * y);
+        double sinfi = y / r;
+        double fi = Math.toDegrees(Math.asin(sinfi));
+
+        if (x1 > x0 && y0 > y1) {
+            fi = 180 - fi;
+        } else if (x1 > x0 && y1 > y0) {
+            fi = 180 - fi;
+        } else if (x0 > x1 && y1 > y0) {
+            fi += 360;
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                if (r > r1 && r < r2) {
+                    if (fi<lastFi) {
+                        rotorAngle -= Math.abs(fi - lastFi) + 0.25f;
+                    } else {
+                        rotorAngle += Math.abs(fi - lastFi) + 0.25f;
+                    }
+                    rotorAngle %= 360;
+                    lastFi = fi;
+                    invalidate();
+                    return true;
+                }
+
+            case MotionEvent.ACTION_DOWN:
+                //rotorAngle = 0;
+                lastFi = fi;
+                return true;
+
+            case MotionEvent.ACTION_UP:
+//                final float angle = rotorAngle % 360;
+//                int number = Math.round(angle - 20) / 30;
+//
+//                if (number > 0) {
+//                    if (number == 10) {
+//                        number = 0;
+//                    }
+//
+//                    fireDialListenerEvent(number);
+//                }
+//
+//                rotorAngle = 0;
+//
+//                post(new Runnable() {
+//                    public void run() {
+//                        float fromDegrees = angle;
+//                        Animation anim = new RotateAnimation(fromDegrees, 0, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//                        anim.setInterpolator(AnimationUtils.loadInterpolator(getContext(), android.R.anim.decelerate_interpolator));
+//                        anim.setDuration((long) (angle * 5L));
+//                        startAnimation(anim);
+//                    }
+//                });
+//
+//                return true;
+            default:
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+}
