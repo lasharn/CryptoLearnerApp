8f881bbe9a9f58ca7f7e4f2449a4975fec39d1bb
monkeyman707 <Monkey_man_707@live.com>
2016-07-05 10:35:46 +1200

    Fixed selected letters for substitution level 2

--- app/src/main/java/substitution_encryption/SubstitutionFrequencyMessage.java
+++ app/src/main/java/substitution_encryption/SubstitutionFrequencyMessage.java
@@ -1,74 +1,146 @@
 package substitution_encryption;
 
+import java.util.Arrays;
+
+import caesar_encryption.CaesarMessage;
 import unpackaged.IMessage;
 
 
 public class SubstitutionFrequencyMessage implements IMessage {
 
     private String eta = "ETA";
-    private IMessage ETA;
-    private IMessage sentence;
-    private int letterCount = 0;
-    private SubstitutionMappings subMap;
+    private String targetWord; // ETA
+    private String answerWord; // ETA encrypted
+    protected String[] selectedCipherLetters;
+    private String targetSentence;
+    private String answerSentence;
+    private String[] selectedSentenceLetters;
 
-    public SubstitutionFrequencyMessage(String targetWord, SubstitutionMappings mappings) {
-        ETA = new SubstitutionMessage(eta, mappings);
-        sentence = new SubstitutionMessage(targetWord, mappings);
-        subMap = mappings;
+
+    public SubstitutionFrequencyMessage(String targetSentence, SubstitutionMappings mappings) {
+        this.targetWord = eta;
+        answerWord = calculateAnswer(this.targetWord, mappings);
+        selectedCipherLetters = new String[this.targetWord.length()];
+        Arrays.fill(selectedCipherLetters, CaesarMessage.emptyAnswerLetter);
+
+        this.targetSentence = targetSentence;
+        answerSentence = calculateAnswer(this.targetSentence, mappings);
+        selectedSentenceLetters = new String[this.targetSentence.length()];
+        Arrays.fill(selectedSentenceLetters, CaesarMessage.emptyAnswerLetter);
+        selectedSentenceLetters = restoreSpaces(selectedSentenceLetters, answerSentence);
     }
 
-
-    @Override
-    public void addLetter(String letter) {
-        if (letterCount >= 3) {
-            return;
+    private String calculateAnswer(String targetWord, SubstitutionMappings mappings) {
+        StringBuilder answer = new StringBuilder();
+        for (char c : targetWord.toCharArray()) {
+            answer.append(mappings.getLetter(Character.toString(c)));
         }
-        ETA.addLetter(letter);
-        // needs to add the corresponding plain text letter TODO doesn't map to correct letter
-        sentence.addLetter(subMap.getLetter(letter));
-        letterCount++;
+
+        return answer.toString();
     }
 
-    @Override
-    public void removeLetter(String letter) {
-        if (letterCount <= 0) {
-            return;
-        }
-        letterCount--;
-        ETA.removeLetter(letter);
-        sentence.removeLetter(Character.toString(eta.charAt(letterCount)));
-    }
-
-    @Override
-    public String getSelectedString() {
-        return ETA.getSelectedString();
-    }
-
-    @Override
-    public boolean isCorrect() {
-        return ETA.isCorrect();
-    }
-
-    @Override
     public String getCorrectAnswer() {
-        return ETA.getCorrectAnswer();
+        return answerWord;
     }
 
-    @Override
-    public String plainTextString() {
-        return ETA.plainTextString();
+    private String[] restoreSpaces(String[] selectedLetters, String answer) {
+        for (int i = 0; i<selectedLetters.length; i++) {
+            char s = answer.charAt(i);
+            if (s == ' ') {
+                selectedLetters[i] = " ";
+            }
+        }
+        return selectedLetters;
     }
 
-    @Override
+    public String getSelectedString() {
+        StringBuilder b = new StringBuilder();
+        for (String s : selectedCipherLetters) {
+            b.append(s);
+        }
+        return b.toString();
+    }
+
+    public void addLetter(String letter) {
+        int positionOfLetter = -1;
+        for (int i = 0; i<selectedCipherLetters.length; i++) {
+            String s = selectedCipherLetters[i];
+            if (s == null || s.isEmpty() || s.equals(CaesarMessage.emptyAnswerLetter)) {
+                positionOfLetter = i;
+                break;
+            }
+        }
+        if (positionOfLetter == -1) {
+            // you have filled everything in. check if correct
+            return;
+        }
+
+        char plainTextLetter = targetWord.charAt(positionOfLetter);
+
+        for (int i = 0; i<selectedCipherLetters.length; i++) {
+            if (targetWord.charAt(i) == plainTextLetter) {
+                selectedCipherLetters[i] = letter;
+            }
+        }
+
+        // deal with full sentence
+        for (int i = 0; i<selectedSentenceLetters.length; i++) {
+            if (answerSentence.charAt(i) == letter.charAt(0)) {
+                selectedSentenceLetters[i] = Character.toString(eta.charAt(positionOfLetter));
+            }
+        }
+    }
+
+    public void removeLetter(String letter) {
+        int positionOfLetter = -1;
+        for (int i = 0; i<selectedCipherLetters.length; i++) {
+            String s = selectedCipherLetters[i];
+            if (s.equals(letter)) {
+                selectedCipherLetters[i] = CaesarMessage.emptyAnswerLetter;
+                positionOfLetter = i;
+            }
+        }
+        if (positionOfLetter == -1) {
+            // letter not in selected letters, something is wrong
+            return;
+        }
+        // deal with full sentence
+        for (int i = 0; i<selectedSentenceLetters.length; i++) {
+            if (selectedSentenceLetters[i].equals(Character.toString(eta.charAt(positionOfLetter)))) {
+                selectedSentenceLetters[i] = CaesarMessage.emptyAnswerLetter;
+            }
+        }
+    }
+
     public boolean isFull() {
-        return ETA.isFull();
+        for (String s : selectedCipherLetters) {
+            if (s.equals(CaesarMessage.emptyAnswerLetter)) {
+                return false;
+            }
+        }
+        return true;
     }
+
+    public boolean isCorrect() {
+        return answerWord.equals(getSelectedString());
+    }
+
+    public String plainTextString() {
+        return targetWord;
+    }
+
+
+
 
     public String selectedSentence() {
-        return sentence.getSelectedString();
+        StringBuilder b = new StringBuilder();
+        for (String s : selectedSentenceLetters) {
+            b.append(s);
+        }
+        return b.toString();
     }
 
     public String answerSentence() {
-        return sentence.getCorrectAnswer();
+        return answerSentence;
     }
 }
